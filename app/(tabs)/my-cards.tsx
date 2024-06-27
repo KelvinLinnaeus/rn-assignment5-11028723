265221e4ceee6fa5c6af84ab35bd77f59867857e
Kelvin Mhacwilson <kelvinlinnaeus@gmail.com>
2024-06-26 21:04:11 -1200

    Added My Cards

--- app/(tabs)/my-cards.tsx
+++ app/(tabs)/my-cards.tsx
@@ -1,10 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
+import Container from "@/components/Container";
 
 export default function MyCards() {
   return (
-    <View>
-      <Text>MyCards</Text>
-    </View>
+    <Container>
+      <Text className="text-center text-xl font-bold my-8">My Cards</Text>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View className="py-2 w-full h-[230px] items-center mb-4">
+          <Image
+            className="h-full w-full"
+            resizeMode="cover"
+            source={require("@/assets/images/Card.png")}
+          />
+        </View>
+        <View className="py-2 w-full h-[230px] items-center mb-4">
+          <Image
+            className="h-full w-full"
+            resizeMode="cover"
+            source={require("@/assets/images/Card.png")}
+          />
+        </View>
+        <View className="py-2 w-full h-[230px] items-center mb-4">
+          <Image
+            className="h-full w-full"
+            resizeMode="cover"
+            source={require("@/assets/images/Card.png")}
+          />
+        </View>
+      </ScrollView>
+    </Container>
   );
 }
